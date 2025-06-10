0bc641ce6a0be2a6d672c18e8895ec365760e043
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-06-10 14:47:15 +0600

    Formatted the code

--- JS/Static-Algorithms/bubbleSort.js
+++ JS/Static-Algorithms/bubbleSort.js
@@ -1,4 +1,4 @@
-export function bubbleSort (array) {
+export function bubbleSort(array) {
     for (let i = 0; i < array.length; i++) {
         for (let j = i + 1; j < array.length; j++) {
             if (array[i] > array[j]) {
