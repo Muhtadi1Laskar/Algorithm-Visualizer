166b9705fe699c594a7b2dfbb8ab56b0edeb38f4
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-06-10 14:47:43 +0600

    Formatted the code

--- JS/Static-Algorithms/gnomeSort.js
+++ JS/Static-Algorithms/gnomeSort.js
@@ -1,17 +1,17 @@
 export function gnomeSort(array) {
     let index = 0;
 
-    while(index < array.length) {
-        if(index === 0) {
+    while (index < array.length) {
+        if (index === 0) {
             index += 1;
         }
-        else if(array[index] >= array[index-1]) {
+        else if (array[index] >= array[index - 1]) {
             index++;
         }
         else {
             let temp = array[index];
-            array[index] = array[index-1];
-            array[index-1] = temp;
+            array[index] = array[index - 1];
+            array[index - 1] = temp;
             index--;
         }
     }
