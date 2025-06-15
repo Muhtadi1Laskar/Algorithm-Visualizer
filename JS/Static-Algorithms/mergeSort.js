528968a7c40f6751fb1828fe77c760d822655892
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-06-15 12:26:40 +0600

    Formatted the code

--- JS/Static-Algorithms/mergeSort.js
+++ JS/Static-Algorithms/mergeSort.js
@@ -1,39 +1,38 @@
 export function merge(left, right) {
-    let merged = [];
-    let i = 0;
-    let j = 0;
+    let merged = []
+    let i = 0
+    let j = 0
 
     while (i < left.length && j < right.length) {
         if (left[i] <= right[j]) {
-            merged.push(left[i]);
-            i += 1;
-        }
-        else {
-            merged.push(right[j]);
-            j += 1;
+            merged.push(left[i])
+            i += 1
+        } else {
+            merged.push(right[j])
+            j += 1
         }
     }
     while (i < left.length) {
-        merged.push(left[i]);
-        i++;
+        merged.push(left[i])
+        i++
     }
     while (j < right.length) {
-        merged.push(right[j]);
-        j++;
+        merged.push(right[j])
+        j++
     }
-    return merged;
+    return merged
 }
 
 export function mergeSort(array) {
     if (array.length <= 1) {
-        return array;
+        return array
     }
-    let mid = Math.floor(array.length / 2);
-    let left = mergeSort(array.slice(0, mid));
-    let right = mergeSort(array.slice(mid));
+    let mid = Math.floor(array.length / 2)
+    let left = mergeSort(array.slice(0, mid))
+    let right = mergeSort(array.slice(mid))
 
-    return merge(left, right);
+    return merge(left, right)
 }
 
-const array = [2, 3, 1, 666, 8, 43, 9, 43, 6];
-console.log(mergeSort(array));
+const array = [2, 3, 1, 666, 8, 43, 9, 43, 6]
+console.log(mergeSort(array))
